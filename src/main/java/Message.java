b786fa9170a95f0f7c529cd4a31479255e7873e9
Баранов Сергей Алексеевич <kretch910@rambler.ru>
2022-04-01 20:13:57 +0300

    добавил аннотации

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,15 +1,21 @@
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Message {
+    @JsonProperty("copyright")
     private String copyright;
+    @JsonProperty("date")
     private String date;
+    @JsonProperty("explanation")
     private String explanation;
+    @JsonProperty("hdurl")
     private String hdurl;
     @JsonProperty("media_type")
     private String mediaType;
     @JsonProperty("service_version")
     private String serviceVersion;
+    @JsonProperty("title")
     private String title;
+    @JsonProperty("url")
     private String url;
 
     public String getUrl() {
